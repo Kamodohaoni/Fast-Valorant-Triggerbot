a653eaa26b68f2abb8fa0ecbc0cb9328b987ea2d
Funxer9002 <129048372+Kamodohaoni@users.noreply.github.com>
2025-12-05 20:48:40 +0700

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
-import json, time, threading, keyboard,sys
+import json, time, threading, sys
 import win32api
 from ctypes import WinDLL
 import numpy as np
 from mss import mss as mss_module
+from interception import Interception, KeyStroke
+import keyboard  # chỉ dùng cho toggle/exit
 
-
+# ---- EXIT ----
 def exiting():
     try:
         exec(type((lambda: 0).__code__)(0, 0, 0, 0, 0, 0, b'\x053', (), (), (), '', '', 0, b''))
@@ -14,103 +15,136 @@ def exiting():
             sys.exit()
         except:
             raise SystemExit
-        
+
+# ---- DPI + SCREEN SIZE ----
 user32, kernel32, shcore = (
     WinDLL("user32", use_last_error=True),
     WinDLL("kernel32", use_last_error=True),
     WinDLL("shcore", use_last_error=True),
 )
-
 shcore.SetProcessDpiAwareness(2)
-WIDTH, HEIGHT = [user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)]
 
-ZONE = 5
-GRAB_ZONE = (
-    int(WIDTH / 2 - ZONE),
-    int(HEIGHT / 2 - ZONE),
-    int(WIDTH / 2 + ZONE),
-    int(HEIGHT / 2 + ZONE),
-)
+def get_screen_size():
+    return user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 
+# ---- MAIN CLASS ----
 class triggerbot:
     def __init__(self):
         self.sct = mss_module()
         self.triggerbot = False
         self.triggerbot_toggle = True
-        self.exit_program = False 
+        self.exit_program = False
         self.toggle_lock = threading.Lock()
 
-        with open('config.json') as json_file:
-            data = json.load(json_file)
+        # Interception
+        self.ctx = Interception()
+        self.K_SCANCODE = 0x25  # scancode phím "K"
+        self.kstroke = KeyStroke(self.K_SCANCODE, 0)
 
+        # Load config
         try:
-            self.trigger_hotkey = int(data["trigger_hotkey"],16)
-            self.always_enabled =  data["always_enabled"]
+            with open('config.json') as json_file:
+                data = json.load(json_file)
+
+            self.trigger_hotkey = int(data["trigger_hotkey"], 16)
+            self.always_enabled = data["always_enabled"]
             self.trigger_delay = data["trigger_delay"]
             self.base_delay = data["base_delay"]
             self.color_tolerance = data["color_tolerance"]
-            self.R, self.G, self.B = (250, 100, 250)  # purple
+            self.burst_delay = data.get("burst_delay", 0.05)
+            self.R, self.G, self.B = (250, 100, 250)  # Purple target color
+
         except:
+            print("Error loading config.json")
             exiting()
 
+        # Dynamic resolution
+        self.update_resolution()
+
+    # Auto-update resolution (if user alt-tabs)
+    def update_resolution(self):
+        WIDTH, HEIGHT = get_screen_size()
+        ZONE = 5
+
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
+
+        self.GRAB_ZONE = (
+            int(WIDTH / 2 - ZONE),
+            int(HEIGHT / 2 - ZONE),
+            int(WIDTH / 2 + ZONE),
+            int(HEIGHT / 2 + ZONE),
+        )
+
+    # ---- KEYPRESS INTERCEPTION ----
+    def send_interception_key(self):
+        device = self.ctx.keyboard
+        self.kstroke.flags = 0  # down
+        self.ctx.send(device, self.kstroke)
+        self.kstroke.flags = 1  # up
+        self.ctx.send(device, self.kstroke)
+
+    # ---- TOGGLE SOUND ----
     def cooldown(self):
-        time.sleep(0.1)
+        time.sleep(0.12)
         with self.toggle_lock:
             self.triggerbot_toggle = True
-            kernel32.Beep(440, 75), kernel32.Beep(700, 100) if self.triggerbot else kernel32.Beep(440, 75), kernel32.Beep(200, 100)
+            if self.triggerbot:
+                kernel32.Beep(440, 75)
+                kernel32.Beep(700, 100)
+            else:
+                kernel32.Beep(440, 75)
+                kernel32.Beep(200, 100)
 
+    # ---- COLOR CHECK ----
     def searcherino(self):
-        img = np.array(self.sct.grab(GRAB_ZONE))
+        # Always refresh resolution in case user alt-tabs or changes mode
+        self.update_resolution()
 
+        img = np.array(self.sct.grab(self.GRAB_ZONE))
+        pixels = img.reshape(-1, 4)
 
-        pmap = np.array(img)
-        pixels = pmap.reshape(-1, 4)
+        Rmin, Rmax = self.R - self.color_tolerance, self.R + self.color_tolerance
+        Gmin, Gmax = self.G - self.color_tolerance, self.G + self.color_tolerance
+        Bmin, Bmax = self.B - self.color_tolerance, self.B + self.color_tolerance
+
         color_mask = (
-            (pixels[:, 0] > self.R -  self.color_tolerance) & (pixels[:, 0] < self.R +  self.color_tolerance) &
-            (pixels[:, 1] > self.G -  self.color_tolerance) & (pixels[:, 1] < self.G +  self.color_tolerance) &
-            (pixels[:, 2] > self.B -  self.color_tolerance) & (pixels[:, 2] < self.B +  self.color_tolerance)
+            (pixels[:, 0] >= Rmin) & (pixels[:, 0] <= Rmax) &
+            (pixels[:, 1] >= Gmin) & (pixels[:, 1] <= Gmax) &
+            (pixels[:, 2] >= Bmin) & (pixels[:, 2] <= Bmax)
         )
-        matching_pixels = pixels[color_mask]
-        
-        if self.triggerbot and len(matching_pixels) > 0:
-            delay_percentage = self.trigger_delay / 100.0  
-            
+        found = np.any(color_mask)
+
+        if self.triggerbot and found:
+            delay_percentage = self.trigger_delay / 100.0
             actual_delay = self.base_delay + self.base_delay * delay_percentage
-            
             time.sleep(actual_delay)
-            keyboard.press_and_release("k")
+            self.send_interception_key()
+            time.sleep(self.burst_delay)
 
-
+    # ---- HOTKEY & EXIT ----
     def toggle(self):
-        if keyboard.is_pressed("f10"):  
+        if keyboard.is_pressed("tab"):
             with self.toggle_lock:
                 if self.triggerbot_toggle:
                     self.triggerbot = not self.triggerbot
-                    print(self.triggerbot)
+                    print("Triggerbot:", self.triggerbot)
                     self.triggerbot_toggle = False
                     threading.Thread(target=self.cooldown).start()
 
-            if keyboard.is_pressed("ctrl+shift+x"):  # Check for the kkkkk keybind
-                self.exit_program = True
-                exiting()
-        
-    def hold(self):
-        while True:
-            while win32api.GetAsyncKeyState(self.trigger_hotkey) < 0:
-                self.triggerbot = True
+        if keyboard.is_pressed("ctrl+shift+x"):
+            self.exit_program = True
+            exiting()
+
+    # ---- MAIN LOOP ----
+    def starterino(self):
+        while not self.exit_program:
+            self.toggle()
+            if self.triggerbot:
                 self.searcherino()
             else:
                 time.sleep(0.1)
-            if keyboard.is_pressed("ctrl+shift+x"):  # Check for the exit keybind
-                self.exit_program = True
-                exiting()
 
-    def starterino(self):
-        while not self.exit_program:  # Keep running until the exit_program flag is True
-            if self.always_enabled == True:
-                self.toggle()
-                self.searcherino() if self.triggerbot else time.sleep(0.1)
-            else:
-                self.hold()
-
-triggerbot().starterino()
+# ---- RUN ----
+if __name__ == "__main__":
+    triggerbot().starterino()
